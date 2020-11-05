8d3a807173c4da66df610d133f8023e6a803570d
Basil Suter <git@nadar.io>
2020-11-05 15:29:05 +0000

    summary block

---
+++ tests/blocks/SummaryBlockTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace luya\forms\tests\blocks;
+
+use luya\forms\blocks\CheckboxesBlock;
+use luya\forms\blocks\SummaryBlock;
+use luya\forms\tests\BlockTestCase;
+use yii\widgets\ActiveField;
+use yii\widgets\ActiveForm;
+
+class SummaryBlockTest extends BlockTestCase
+{
+    public $blockClass = SummaryBlock::class;
+
+    public function testFrontend()
+    {
+        $this->app->forms->startForm(ActiveForm::begin());
+        $this->block->setVarValues([
+            'attribute' => 'firstname',
+            'values' => [],
+        ]);
+        $this->assertEmpty($this->block->frontend());
+        $this->assertNotEmpty($this->block->admin());
+        $this->assertNotEmpty($this->block->icon());
+        $this->assertNotEmpty($this->block->name());
+        $this->assertNotEmpty($this->block->blockGroup());
+        $this->assertNotEmpty($this->block->config());
+
+        $this->app->forms->form->end();
+    }
+}
